ea4d6a2fe7f8fd655d5b969c29f412950243ddb1
minkimcello <minkimcello@gmail.com>
2019-08-29 18:00:52 -0400

    123123

--- .github/auto-commit/entrypoint.sh
+++ .github/auto-commit/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-sh -c "git add -A && git commit -m '$*' --allow-empty \
-      && git push -u origin HEAD"
+sh -c "echo "`node -e \"console.log(require('./package.json').version)\"`"&& git add -A && git commit -m '$*' --allow-empty && git push -u origin HEAD"
