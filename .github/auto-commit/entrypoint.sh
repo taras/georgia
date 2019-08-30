557452b95d5b5fb7097d7a97491cc41820375017
minkimcello <minkimcello@gmail.com>
2019-08-29 22:16:25 -0400

    12

--- .github/auto-commit/entrypoint.sh
+++ .github/auto-commit/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-sh -c touch dingdong.txt && git remote set-url origin https://$GITHUB_TOKEN:github.com/taras/georgia.git && git add -A && git commit -m '$*' --allow-empty && git push -u origin HEAD
+sh -c git remote set-url origin https://$GITHUB_TOKEN:github.com/taras/georgia.git && echo hello > test.txt && git add test.txt && git commit -m '$*' --allow-empty && git push -u origin HEAD
