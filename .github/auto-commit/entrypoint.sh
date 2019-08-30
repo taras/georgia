e867941cc118daaf3b39fa989c9766305425b121
minkimcello <minkimcello@gmail.com>
2019-08-29 23:14:50 -0400

    yeah

--- .github/auto-commit/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-sh -c "git config user.email 'minkimcello@gmail.com' && git config user.name 'Min Kim' && echo hello > test.txt && git add test.txt && git commit -m '$*' --allow-empty && git push -u origin HEAD"
