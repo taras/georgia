9f0faf2cb25b579a372144350a21702493ad6daa
minkimcello <minkimcello@gmail.com>
2019-08-29 22:05:19 -0400

    12

--- .github/auto-commit/entrypoint.sh
+++ .github/auto-commit/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -e
 
-# sh -c "git config --global user.email "minkimcello@gmail.com" && git config --global user.name "minkimcello" && git add -A && git commit -m '$*' --allow-empty && git push -u origin HEAD"
-
-sh -c echo hello && echo bye && echo what
+sh -c git config --global user.email "minkimcello@gmail.com" && git config --global user.name "minkimcello" && git add -A && git commit -m '$*' --allow-empty && git push -u origin HEAD"
