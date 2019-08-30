7bcd058e9a8a4947d532e8ff183c9f5f63e0d9cb
minkimcello <minkimcello@gmail.com>
2019-08-29 22:19:10 -0400

    12

--- .github/auto-commit/entrypoint.sh
+++ .github/auto-commit/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-sh -c git remote set-url origin https://$GITHUB_TOKEN:github.com/taras/georgia.git && echo hello > test.txt && git add test.txt && git commit -m '$*' --allow-empty && git push -u origin HEAD
+sh -c git config user.name minkimcello && git config user.email minkimcello@gmail.com && echo hello > test.txt && git add test.txt && git commit -m '$*' --allow-empty && git push -u origin HEAD
