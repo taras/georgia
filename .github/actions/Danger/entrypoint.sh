853b774cd9e9e32ccf7745ed26a15f5568c472c9
minkimcello <minkimcello@gmail.com>
2019-09-05 17:35:28 -0400

    checksecret

--- .github/actions/Danger/entrypoint.sh
+++ .github/actions/Danger/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+if [[ -z "$DANGER_GITHUB" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
 cd .github
 yarn global add danger --dev
 export PATH="$(yarn global bin):$PATH"
-export GITHUB_TOKEN={$DANGER_GITHUB}
 danger ci
