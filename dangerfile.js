839f7e9869ab5dd0748b81669a05931a4c68f0f3
minkimcello <minkimcello@gmail.com>
2019-09-06 11:01:32 -0400

    perhaps

--- dangerfile.js
+++ dangerfile.js
@@ -6,9 +6,12 @@ const shorted = danger.github.commits[latest].sha.slice(0, 7);
 
 const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
 
-markdown(`This PR is available to use:`);
-markdown('```bash');
-markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
-markdown('```');
-markdown(`You can view the NPM package [here](${currentNPM}).`);
-markdown('weee weee wee');
+// markdown(`This PR is available to use:`);
+// markdown('```bash');
+// markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
+// markdown('```');
+// markdown(`You can view the NPM package [here](${currentNPM}).`);
+// markdown('weee weee wee');
+
+
+markdown('meh');
