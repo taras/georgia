7dc72cc537e02a149623076fb4cc5447ae7ccb4a
minkimcello <minkimcello@gmail.com>
2019-09-06 13:37:09 -0400

    oh

--- .github/actions/Danger/entrypoint.sh
+++ .github/actions/Danger/entrypoint.sh
@@ -6,4 +6,4 @@ yarn global add danger --dev
 export PATH="$(yarn global bin):$PATH"
 #cp ~/dangerfile.js /dingding.js
 #cd ../ && cp dangerfile.js .github/dangerfile.js && echo weeeeeeeeeee && cd .github && ls
-danger ci --dangerfile ../dangerfile.js
+danger ci --dangerfile ./../dangerfile.js
