f66faa9667bd30224c1aeef7c64022fa1aeb350e
minkimcello <minkimcello@gmail.com>
2019-09-06 12:20:23 -0400

    all3

--- .github/actions/Danger/OLD_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd .github
-GITHUB_TOKEN=$DANGER_GITHUB yarn global add danger --dev
-export PATH="$(yarn global bin):$PATH"
-danger ci
-# $token danger ci
--- .github/dangerfile.js
+++ .github/dangerfile.js
@@ -6,11 +6,8 @@ const shorted = danger.github.commits[latest].sha.slice(0, 7);
 
 const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
 
-// markdown(`This PR is available to use:`);
-// markdown('```bash');
-// markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
-// markdown('```');
-// markdown(`You can view the NPM package [here](${currentNPM}).`);
-
-
-markdown('waaa succssess! 5???')
+markdown(`This PR is available to use:`);
+markdown('```bash');
+markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
+markdown('```');
+markdown(`You can view the NPM package [here](${currentNPM}).`);
