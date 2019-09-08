d8c83d175dc81418c0fa3ecb59af59c6c7c65cf6
minkimcello <minkimcello@gmail.com>
2019-09-08 02:27:49 -0400

    goback

--- .github/actions/Beta/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
-
-npm publish --access=public
