3bdae312d86238de7c4938a35b1c35af8918f776
minkimcello <minkimcello@gmail.com>
2019-09-09 16:24:27 -0400

    again

--- .github/actions/Packager/entrypoint.sh
+++ .github/actions/Packager/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`"
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
