65101e0f8fb8141ddbd8d51980999c287c768de5
minkimcello <minkimcello@gmail.com>
2019-09-09 16:21:24 -0400

    trypublishing

--- .github/actions/Packager/entrypoint.sh
+++ .github/actions/Packager/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`"
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`"
