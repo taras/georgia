b49a85f68ce96540538d1cb146ba0c4e7be370c1
minkimcello <minkimcello@gmail.com>
2019-09-10 16:09:56 -0400

    hello with argument

--- .github/actions/Filter/entrypoint.sh
+++ .github/actions/Filter/entrypoint.sh
@@ -13,12 +13,15 @@ else
   
   # if [[ $(echo $((git log -1 --pretty=%B) | grep "hello")) ]]
   ## doesn't work
-  if [[ $(echo "$($INPUT_ARGUMENT)" | grep $INPUT_FILTER) ]]
-    then
-      echo Filter triggered and halting the rest of the workflow.
-      echo $($INPUT_ARGUMENT)
-      exit 1
-    else
-      echo Filter not triggered. Resuming workflow.
-  fi
+  compute="${INPUT_ARGUMENT}"
+  echo $compute
+  exit 1
+  # if [[ $(echo "$($INPUT_ARGUMENT)" | grep $INPUT_FILTER) ]]
+  #   then
+  #     echo Filter triggered and halting the rest of the workflow.
+  #     echo $($INPUT_ARGUMENT)
+  #     exit 1
+  #   else
+  #     echo Filter not triggered. Resuming workflow.
+  # fi
 fi
