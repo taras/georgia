4dfaa3df8f464894e7743515cbe2230f88410294
minkimcello <minkimcello@gmail.com>
2019-09-10 12:17:36 -0400

    testerrormessage

--- .github/actions/Filter/entrypoint.sh
+++ .github/actions/Filter/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-echo MY ERROR MESSAGE
-exit 0
-
-echo "$INPUT_PARAMETER"
-echo "$INPUT_ARGUMENT"
+if [[ "${#INPUT_PARAMETER}" -ne "0" ]]
+then
+  echo Error: Please include a \`parameter:\` under \`with:\`.
+  exit 1
+elif [[ "${#INPUT_ARGUMENT}" -eq "0" ]]
+then
+  echo Error: Please include a \`parameter:\` under \`with:\`.
+  exit 1
+else
+  echo "$INPUT_PARAMETER"
+  echo "$INPUT_ARGUMENT"
+fi
