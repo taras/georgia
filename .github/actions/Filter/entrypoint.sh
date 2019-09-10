a6c4f182817605e46de0d6aedd23b6ae56975a19
minkimcello <minkimcello@gmail.com>
2019-09-10 12:28:47 -0400

    skip

--- .github/actions/Filter/entrypoint.sh
+++ .github/actions/Filter/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-if [[ "${#INPUT_PARAMETER}" -eq "0" ]]
-then
+if [[ "${#INPUT_PARAMETER}" -eq "0" ]]; then
   echo Error: Please include a \`parameter:\` under \`with:\`.
   exit 1
-elif [[ "${#INPUT_ARGUMENT}" -eq "0" ]]
-then
+elif [[ "${#INPUT_ARGUMENT}" -eq "0" ]]; then
   echo Error: Please include a \`parameter:\` under \`with:\`.
   exit 1
 else
-  echo $($INPUT_PARAMETER)
-#  if [[ "$(git log -1 --pretty=%B)" =~ "[ci skip]" ]]
+  if [[ $($INPUT_PARAMETER) =~ skip ]]; then
+    echo yes
+    exit 1; else
+    echo ohno;
+  fi
 fi
