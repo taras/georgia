79e246d52248a93eaf4e3e200fe2a4da4108df7f
minkimcello <minkimcello@gmail.com>
2019-09-10 16:18:06 -0400

    yeah

--- .github/actions/Filter/entrypoint.sh
+++ .github/actions/Filter/entrypoint.sh
@@ -4,24 +4,22 @@ IFS=$'\n\t'
 
 if [[ "${#INPUT_FILTER}" -eq "0" ]]
   then
-    echo Error: Please include an argument.
+    echo Error: Please include a filter.
     exit 1
 else
-  
-  # if [[ $(echo $(git log -1 --pretty=%B) | grep "hello") ]]
-  ## above works
+  if [[ $(echo $(git log -1 --pretty=%B) | grep "$INPUT_FILTER") ]]
+  ## works
   
   # if [[ $(echo $((git log -1 --pretty=%B) | grep "hello")) ]]
   ## doesn't work
-  compute=$($INPUT_ARGUMENT)
-  echo $compute
-  exit 1
+
   # if [[ $(echo "$($INPUT_ARGUMENT)" | grep $INPUT_FILTER) ]]
-  #   then
-  #     echo Filter triggered and halting the rest of the workflow.
-  #     echo $($INPUT_ARGUMENT)
-  #     exit 1
-  #   else
-  #     echo Filter not triggered. Resuming workflow.
-  # fi
+  ## doesn't work
+  
+    then
+      echo Filter triggered and halting the rest of the workflow.
+      exit 1
+    else
+      echo Filter not triggered. Resuming workflow.
+  fi
 fi
