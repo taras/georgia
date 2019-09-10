42448e51766abd1430b468a2c057dd98138b7dcb
minkimcello <minkimcello@gmail.com>
2019-09-10 14:24:51 -0400

    grep5

--- .github/actions/Filter/entrypoint.sh
+++ .github/actions/Filter/entrypoint.sh
@@ -9,9 +9,11 @@ elif [[ "${#INPUT_ARGUMENT}" -eq "0" ]]; then
   echo Error: Please include a \`parameter:\` under \`with:\`.
   exit 1
 else
-  if [[ $(echo "hello world" | grep "world") ]]; then
-    echo "It's there!"; else
-    echo no
+  if [[ $(echo "hello world" | grep "world") ]]
+    then
+      echo "It's there!"
+    else
+      echo "no"
   fi
 
 #!/bin/bash
