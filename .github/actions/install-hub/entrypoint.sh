0b8c9b929691d73c8f8c6c76b191a663927b5f77
minkimcello <minkimcello@gmail.com>
2019-09-11 10:59:46 -0400

    labels

--- .github/actions/install-hub/entrypoint.sh
+++ .github/actions/install-hub/entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 wget https://github.com/github/hub/releases/download/v2.12.3/hub-linux-amd64-2.12.3.tgz
 tar -zxvf hub-linux-amd64-2.12.3.tgz
 cd hub-linux-amd64-2.12.3
-sudo ./install && cd .. && ls
+sudo ./install && cd ..
+labels=$(hub pr list -s merged -L 1 -f %L)
 
-
-# for the purpose of:
-#labels=$(hub pr list -s merged -L 1 -f %L)
 #echo $labels
