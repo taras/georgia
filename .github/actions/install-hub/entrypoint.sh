ca4e6b62e3f740fb6ca5a2b99ca60d28995a7a5b
minkimcello <minkimcello@gmail.com>
2019-09-11 11:26:14 -0400

    stack

--- .github/actions/install-hub/entrypoint.sh
+++ .github/actions/install-hub/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+su -c "setenforce 0"
 wget https://github.com/github/hub/releases/download/v2.12.3/hub-linux-amd64-2.12.3.tgz
 tar -zxvf hub-linux-amd64-2.12.3.tgz
 cd hub-linux-amd64-2.12.3
