32740fe8942a0a262ad2fdd63a2192d8d86dbd50
minkimcello <minkimcello@gmail.com>
2019-09-11 10:51:17 -0400

    cd

--- .github/actions/install-hub/entrypoint.sh
+++ .github/actions/install-hub/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 wget https://github.com/github/hub/releases/download/v2.12.3/hub-linux-amd64-2.12.3.tgz
 tar -zxvf hub-linux-amd64-2.12.3.tgz
 cd hub-linux-amd64-2.12.3
-sudo ./install
+sudo ./install && cd ..
 
 
 # for the purpose of:
