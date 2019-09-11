e9f7ed31bd1c2c386c865c04313647e7c6f02e57
minkimcello <minkimcello@gmail.com>
2019-09-11 13:14:33 -0400

    may

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,10 +1,10 @@
 //const core = require('@actions/core');
+const core = require('@actions/core');
 
 async function run() {
   try {
-    // const nameInput = core.getInput('name');
-    // console.log(`Hello ${nameInput}!`);
-    console.log('hello')
+    const nameInput = core.getInput('name');
+    console.log(`Hello ${nameInput}!`);
   } catch (error) {
     //core.setFailed(error.message);
     console.log('error')
