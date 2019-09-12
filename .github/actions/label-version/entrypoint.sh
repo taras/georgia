29add8204116309c7d0d40a13cc383e8376d9a6d
minkimcello <minkimcello@gmail.com>
2019-09-12 13:38:50 -0400

    notliterally

--- .github/actions/label-version/entrypoint.sh
+++ .github/actions/label-version/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $PR_LABELS
+echo $($PR_LABELS)
 
 # npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
