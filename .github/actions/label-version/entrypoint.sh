6b16026d02ebac0907214908302f429cfbaddb63
minkimcello <minkimcello@gmail.com>
2019-09-12 14:05:14 -0400

    newenv

--- .github/actions/label-version/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo $PR_LABELS
-
-# npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
