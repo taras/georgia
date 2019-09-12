18d28b8e475ca833311147ce1d600918bec38913
minkimcello <minkimcello@gmail.com>
2019-09-12 18:31:11 -0400

    withoutthenonsense

--- .github/actions/git-push/entrypoint.sh
+++ .github/actions/git-push/entrypoint.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 # git setup
-git remote set-url origin https://$INPUT_GITHUB_TOKEN:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
-git fetch origin +refs/heads/*:refs/heads/*
-branch=$(printf "%s\n" "${GITHUB_REF#*refs\/heads\/}")
-git checkout $branch
+#git remote set-url origin https://$INPUT_GITHUB_TOKEN:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
+#git fetch origin +refs/heads/*:refs/heads/*
+#branch=$(printf "%s\n" "${GITHUB_REF#*refs\/heads\/}")
+#git checkout $branch
 # git_config_email="`node -e \"console.log(require('./package.json').author.email)\"`"
 # git_config_name="`node -e \"console.log(require('./package.json').author.name)\"`"
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
