7b63ff1a2e09174b41afeb18c7875bef23d9d048
minkimcello <minkimcello@gmail.com>
2019-09-12 18:19:17 -0400

    ohboy

--- .github/actions/git-push/entrypoint.sh
+++ .github/actions/git-push/entrypoint.sh
@@ -6,10 +6,10 @@ git remote set-url origin https://$INPUT_GITHUB_TOKEN:x-oauth-basic@github.com/$
 git fetch origin +refs/heads/*:refs/heads/*
 branch=$(printf "%s\n" "${GITHUB_REF#*refs\/heads\/}")
 git checkout $branch
-git_config_email="`node -e \"console.log(require('./package.json').author.email)\"`"
-git_config_name="`node -e \"console.log(require('./package.json').author.name)\"`"
-git config user.email $git_config_email
-git config user.name $git_config_name
+# git_config_email="`node -e \"console.log(require('./package.json').author.email)\"`"
+# git_config_name="`node -e \"console.log(require('./package.json').author.name)\"`"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config user.name "$GITHUB_ACTOR"
 git add $INPUT_ADD
 current="`node -e \"console.log(require('./package.json').version)\"`"
 git commit -m "Release version $current [skip-actions]"
