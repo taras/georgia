5d998989434c324236df0ee21bd98fa4eb95d895
minkimcello <minkimcello@gmail.com>
2019-09-12 14:49:47 -0400

    onceagain

--- .github/actions/git-push/entrypoint.sh
+++ .github/actions/git-push/entrypoint.sh
@@ -10,7 +10,7 @@ git_config_email="`node -e \"console.log(require('./package.json').author.email)
 git_config_name="`node -e \"console.log(require('./package.json').author.name)\"`"
 git config user.email $git_config_email
 git config user.name $git_config_name
-git add package.json
+git add $INPUT_ADD
 current="`node -e \"console.log(require('./package.json').version)\"`"
 git commit -m "Release version $current [skip-actions]"
 git push origin HEAD
