fecb2d22ebd625ed0ab384d9b9c638adbcf4f2a2
minkimcello <minkimcello@gmail.com>
2019-09-16 17:27:48 -0400

    clean up comments. skip-actions

--- .github/actions/filter-by-commit/entrypoint.sh
+++ .github/actions/filter-by-commit/entrypoint.sh
@@ -7,25 +7,11 @@ if [[ "${#INPUT_FILTER}" -eq "0" ]]
     echo Error: Please include a filter.
     exit 1
 else  
-  # if [[ $(echo "$(git log -1 --pretty=%B)" | grep "hello") ]]
-  ## above works
-
-  if [[ $(echo "$(git log -1 --pretty=%B)" | grep "$INPUT_FILTER") ]]
-  ## testing
-  
-  # if [[ $(echo $((git log -1 --pretty=%B) | grep "hello")) ]]
-  ## doesn't work
-
-  # if [[ $(echo "$($INPUT_ARGUMENT)" | grep $INPUT_FILTER) ]]
-  ## doesn't work
-  
+  if [[ $(echo "$(git log -1 --pretty=%B)" | grep "$INPUT_FILTER") ]] 
     then
       echo Filter triggered and halting the rest of the workflow.
-      #echo $($INPUT_ARGUMENT)
       exit 1
     else
       echo Filter not triggered. Resuming workflow.
   fi
 fi
-
-## very messy. cleanup inbound.
--- .github/actions/git-push/entrypoint.sh
+++ .github/actions/git-push/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-# git setup
 git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
 git fetch origin +refs/heads/*:refs/heads/*
 branch=$(printf "%s\n" "${GITHUB_REF#*refs\/heads\/}")
 git checkout $branch
-# git_config_email="`node -e \"console.log(require('./package.json').author.email)\"`"
-# git_config_name="`node -e \"console.log(require('./package.json').author.name)\"`"
+
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git config user.name "$GITHUB_ACTOR"
+
 git add $INPUT_ADD
+
 current="`node -e \"console.log(require('./package.json').version)\"`"
 git commit -m "Release version $current [skip-actions]"
-# git push origin HEAD
 git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" $branch
