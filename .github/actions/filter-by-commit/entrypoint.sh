0ad1849c4458ef8a3a9d2372e63a3397759aff7c
minkimcello <minkimcello@gmail.com>
2019-09-17 19:32:52 -0400

    ext #123

--- .github/actions/filter-by-commit/entrypoint.sh
+++ .github/actions/filter-by-commit/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ "${#INPUT_FILTER}" -eq "0" ]]
     echo Error: Please include a filter.
     exit 1
 else  
-  if [[ $(echo "$(git log -1 --pretty=%B)" | grep -e "#(\d+)") ]] 
+  if [[ $(echo "$(git log -1 --pretty=%B)" | grep -E "#(\d+)") ]] 
     then
       echo Resuming the rest of the workflow.
     else
