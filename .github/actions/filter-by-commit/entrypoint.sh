cf13ec735f9627cc4650e580cc31651f64d99bd5
minkimcello <minkimcello@gmail.com>
2019-09-17 19:38:13 -0400

    yeah #123

--- .github/actions/filter-by-commit/entrypoint.sh
+++ .github/actions/filter-by-commit/entrypoint.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ "${#INPUT_FILTER}" -eq "0" ]]
-  then
-    echo Error: Please include a filter.
-    exit 1
-else  
-  if [[ $(echo "$(git log -1 --pretty=%B)" | grep ${INPUT_FILTER}) ]] 
+# if [[ "${#INPUT_FILTER}" -eq "0" ]]
+#   then
+#     echo Error: Please include a filter.
+#     exit 1
+# else  
+  if [[ $(echo "$(git log -1 --pretty=%B)" | grep -E "#(\d+)") ]] 
     then
       echo Resuming the rest of the workflow.
     else
       echo Halting the rest of the workflow.
       exit 1
   fi
-fi
+# fi
