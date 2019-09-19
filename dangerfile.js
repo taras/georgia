0ceded7b147b37b8ed8214a955da75fc896b2621
minkimcello <minkimcello@gmail.com>
2019-09-19 12:41:35 -0400

    sha danger

--- dangerfile.js
+++ dangerfile.js
@@ -4,8 +4,7 @@ const pjson = require('./package.json');
 const latest = danger.github.pr.commits - 1;
 const shorted = danger.github.commits[latest].sha.slice(0, 7);
 
-console.log(danger.github.commits[0])
-console.log(danger.github.commits[1])
+console.log("sha", danger.github.commits[0].sha)
 
 const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
 
