fae485701714b4ee416b519013b97bfe87598c2b
minkimcello <minkimcello@gmail.com>
2019-09-19 12:34:09 -0400

    update danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
 const { danger, markdown } = require('danger');
 const pjson = require('./package.json');
 
-const latest = danger.github.pr.commits - 1;
+const latest = danger.github.pr.commits;
 const shorted = danger.github.commits[latest].sha.slice(0, 7);
 
 const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
