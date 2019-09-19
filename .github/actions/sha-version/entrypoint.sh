7e35903306d70d178826f8ea7dc059d48a8618a5
minkimcello <minkimcello@gmail.com>
2019-09-19 14:27:19 -0400

    update shama

--- .github/actions/sha-version/entrypoint.sh
+++ .github/actions/sha-version/entrypoint.sh
@@ -3,4 +3,6 @@ set -e
 
 echo $GITHUB_SHA
 
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
+# npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1 --skip 1`" --no-git-tag-version
+
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git rev-parse --short ${GITHUB_SHA}`" --no-git-tag-version
