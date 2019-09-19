96fbcf920347d64e17bf5862a1a743279471d3e0
minkimcello <minkimcello@gmail.com>
2019-09-19 13:43:59 -0400

    original

--- .github/actions/git-push/entrypoint.sh
+++ .github/actions/git-push/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-# git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
-# git fetch origin +refs/heads/*:refs/heads/*
+git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
+git fetch origin +refs/heads/*:refs/heads/*
 branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
-# git checkout $branch
+git checkout $branch
+
+echo branch: $branch
 
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git config user.name "$GITHUB_ACTOR"
 
 git add $INPUT_ADD
-
-echo $GITHUB_BASE_REF
 
 current="`node -e \"console.log(require('./package.json').version)\"`"
 git commit -m "Release version $current"
