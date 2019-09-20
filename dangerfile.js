f5d14fed812fcd9a76a07c423bba8d4122db5d67
minkimcello <minkimcello@gmail.com>
2019-09-20 17:31:31 -0400

    weekend cleanup

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,5 @@
-const { danger, markdown } = require('danger');
+const { markdown } = require('danger');
 const pjson = require('./package.json');
-
-// const latest = danger.github.pr.commits - 1;
-// const shorted = danger.github.commits[latest].sha.slice(0, 7);
 
 const shorted = process.env.GITHUB_SHA.slice(0, 7);
 
@@ -12,4 +9,4 @@ markdown(`This PR is available to use:`);
 markdown('```bash');
 markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
 markdown('```');
-markdown(`You can view the NPM package [here](${currentNPM}).`);  
+markdown(`You can view the NPM package [here](${currentNPM}).`);
