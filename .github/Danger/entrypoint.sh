c4eff17e280300c163300ff5b133cf2897a2fade
minkimcello <minkimcello@gmail.com>
2019-09-24 15:38:31 -0400

    oh yeah

--- .github/Danger/entrypoint.sh
+++ .github/Danger/entrypoint.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 set -e
 
-# cat > dangerfile.js <<- EOL
-# const { markdown } = require('danger');
-# const pjson = require('./package.json');
-
-# const shorted = process.env.GITHUB_SHA.slice(0, 7);
-
-# const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
-
-# markdown(`This PR is available to use:`);
-# markdown('```bash');
-# markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
-# markdown('```');
-# markdown('in action');
-# markdown(`You can view the NPM package [here](${currentNPM}).`);
-# EOL
-
-cat > dangerfile.js <<- EOL
+cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
-markdown('hello');
-EOL
+const pjson = require('./package.json');
+
+const shorted = process.env.GITHUB_SHA.slice(0, 7);
+
+const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
+
+markdown(`This PR is available to use:`);
+markdown('```bash');
+markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
+markdown('```');
+markdown('in action');
+markdown(`You can view the NPM package [here](${currentNPM}).`);
+EOT
 
 # cd .github
 yarn global add danger --dev
