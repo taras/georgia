a4c1b927ff63e7f3e4ae76706a9dad952473be5f
minkimcello <minkimcello@gmail.com>
2019-09-24 12:13:12 -0400

    two ls

--- .github/Danger/entrypoint.sh
+++ .github/Danger/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 ls
 
 # cd .github
-yarn global add danger --dev
-export PATH="$(yarn global bin):$PATH"
-danger ci
+# yarn global add danger --dev
+# export PATH="$(yarn global bin):$PATH"
+# danger ci
