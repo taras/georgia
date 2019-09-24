1f663fb820b0edc45bcbdbf9785ba3eeb255cb17
minkimcello <minkimcello@gmail.com>
2019-09-24 12:48:44 -0400

    from source

---
+++ dangerfile.js
@@ -0,0 +1,12 @@
+const { markdown } = require('danger');
+const pjson = require('./package.json');
+
+const shorted = process.env.GITHUB_SHA.slice(0, 7);
+
+const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}-${shorted}`
+
+markdown(`This PR is available to use:`);
+markdown('```bash');
+markdown(`npm install ${pjson.name}@${pjson.version}-${shorted}`);
+markdown('```');
+markdown(`You can view the NPM package [here](${currentNPM}).`);
