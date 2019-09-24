a95a747c3734041172accda244caf9d1c53fef1a
minkimcello <minkimcello@gmail.com>
2019-09-24 11:22:10 -0400

    run in entrypoint

--- .github/Danger/entrypoint.sh
+++ .github/Danger/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-#cd .github
-# yarn global add danger --dev
-# export PATH="$(yarn global bin):$PATH"
-# danger ci
-
-ls
+# cd .github
+yarn global add danger --dev
+export PATH="$(yarn global bin):$PATH"
+danger ci
