65802f4f565e8fb12128d779904be8477e29b5a9
minkimcello <minkimcello@gmail.com>
2019-10-08 16:50:34 -0400

    echo branch

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -7,9 +7,10 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-results=[]
+results=()
 for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
-    results+="${$branch#*refs\/heads\/}"
+    results+=("${$branch#*refs\/heads\/}")
+    echo "${$branch#*refs\/heads\/}"
 done
 echo before
 echo $results
