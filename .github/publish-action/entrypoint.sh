856ffdfca058bc5d7bce9ef5c27687a14e144c33
minkimcello <minkimcello@gmail.com>
2019-10-08 13:56:52 -0400

    without bash in markdown

--- .github/publish-action/entrypoint.sh
+++ .github/publish-action/entrypoint.sh
@@ -25,10 +25,10 @@ const pjson = require('./package.json');
 
 const currentNPM = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}`
 
-markdown(`This PR is available to use:`);
-markdown('```bash');
-markdown(`npm install ${pjson.name}@${pjson.version}`);
-markdown('```');
+//markdown(`This PR is available to use:`);
+//markdown('```bash');
+//markdown(`npm install ${pjson.name}@${pjson.version}`);
+//markdown('```');
 markdown(`You can view the NPM package [here](${currentNPM}).`);
 EOT
 
