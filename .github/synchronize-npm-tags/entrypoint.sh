751a631ed115c2e10089f75efc2a33e1f1a6fc8e
minkimcello <minkimcello@gmail.com>
2019-10-08 16:39:15 -0400

    see ls-remote and payload

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -7,6 +7,8 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
+git ls-remote
+
+for branch in $(git for-each-ref --format='%(refname)' refs/remotes/origin/); do
     git log --oneline "$branch" ^origin/master
 done
