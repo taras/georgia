1742b765cbbf846adf6cfd7f38ea9f6d2091ce68
minkimcello <minkimcello@gmail.com>
2019-10-08 16:27:34 -0400

    try without checkout

---
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+IFS=$'\n\t'
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
+    #git log --oneline "$branch" ^origin/master
+    echo $branch
+done
