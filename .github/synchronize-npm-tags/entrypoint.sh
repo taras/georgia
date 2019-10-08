a5d8fb30438e72cb9eae67d6eebde5bf917d1dc6
minkimcello <minkimcello@gmail.com>
2019-10-08 17:07:28 -0400

    try loop

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -7,9 +7,11 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-for branch in $(git ls-remote --heads); do
+results=()
+for branch in $(git ls-remote --heads origin  | sed 's?.*refs/heads/??'); do
     #echo "${$branch#*refs\/heads\/}"
     echo $branch
+    results+=("$branch")
 done
 echo before
 echo $results
