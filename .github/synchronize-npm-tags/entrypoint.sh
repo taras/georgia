a3cdb5acf77f553f9e85c741d0197f0843aa7c54
minkimcello <minkimcello@gmail.com>
2019-10-09 00:00:29 -0400

    test-tagging

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 IFS=$'\n\t'
 
@@ -6,8 +6,12 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+    #echo "${$branch#*refs\/heads\/}"
 
 for branch in $(git ls-remote --heads origin  | sed 's?.*refs/heads/??'); do
-    #echo "${$branch#*refs\/heads\/}"
-    echo $branch
+    echo $branch | grep -oe "features"
+done
+
+for tag in $(npm dist-tag ls | grep -E ".+?(?=':')"); do 
+  echo $tag 
 done
