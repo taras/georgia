09447d527d370101953600d50d082542e1f58977
minkimcello <minkimcello@gmail.com>
2019-10-09 13:23:56 -0400

    safe run to keep features-2

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -8,23 +8,20 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 package="`node -e \"console.log(require('./package.json').name)\"`"
 
-# one time
-echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-npm publish --tag features-2
 
-# for tag in $(npm dist-tag ls | sed 's/\:.*//'); do
-#   if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
-#     then
-#       echo -e "${GREEN}Skipping protected tag, ${YELLOW}$tag${GREEN}."
-#   elif [[ $(echo "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')" | grep -e "$tag") ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, on NPM because we found a matching branch.${NC}"
-#   else
-#     #echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-#     npm dist-tag rm $package $tag
-#     echo -e "${YELLOW}Removed tag, ${RED}$tag${YELLOW}, from NPM because it did not match any existing branches on HEAD.${NC}"
-#   fi
-# done
+for tag in $(npm dist-tag ls | sed 's/\:.*//'); do
+  if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
+    then
+      echo -e "${GREEN}Skipping protected tag, ${YELLOW}$tag${GREEN}."
+  elif [[ $(echo "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')" | grep -e "$tag") ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, on NPM because we found a matching branch.${NC}"
+  else
+    #echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    #npm dist-tag rm $package $tag
+    echo -e "${YELLOW}Removed tag, ${RED}$tag${YELLOW}, from NPM because it did not match any existing branches on HEAD.${NC}"
+  fi
+done
 
 # npm tags
     # beta = delete
