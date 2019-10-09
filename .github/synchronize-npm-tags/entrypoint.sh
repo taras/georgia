06da470d99ea73f4be8257267712f6b41bee59ad
minkimcello <minkimcello@gmail.com>
2019-10-09 13:17:24 -0400

    push tag to npm

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -8,21 +8,23 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 package="`node -e \"console.log(require('./package.json').name)\"`"
 
+# one time
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+npm dist-tag add @minkimcello/georgia@release-1.0.0 features-2
+
 # for tag in $(npm dist-tag ls | sed 's/\:.*//'); do
 #   if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
 #     then
 #       echo -e "${GREEN}Skipping protected tag, ${YELLOW}$tag${GREEN}."
-#   elif [[ $(echo "$tag" | grep -e "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')") ]]
+#   elif [[ $(echo "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')" | grep -e "$tag") ]]
 #     then
 #       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, on NPM because we found a matching branch.${NC}"
 #   else
-#     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+#     #echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 #     npm dist-tag rm $package $tag
 #     echo -e "${YELLOW}Removed tag, ${RED}$tag${YELLOW}, from NPM because it did not match any existing branches on HEAD.${NC}"
 #   fi
 # done
-
-echo $(echo "features-2" | grep -e "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')")
 
 # npm tags
     # beta = delete
@@ -36,12 +38,3 @@ echo $(echo "features-2" | grep -e "$(git ls-remote --heads origin  | sed 's?.*r
     # master
     # release-1.0.0
     # release-2.0.0
-
-
-    # 6cdc57e
-
-
-    
-
-
-# features features-2 master release-1.0.0 release-2.0.0
