a1645580530b91f39cd60f01639de0c3140d329c
minkimcello <minkimcello@gmail.com>
2019-10-09 13:45:04 -0400

    echo and rerun

--- .github/synchronize-npm-tags/entrypoint.sh
+++ .github/synchronize-npm-tags/entrypoint.sh
@@ -7,14 +7,20 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m'
+
 package="`node -e \"console.log(require('./package.json').name)\"`"
+branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')"
+npmtags=$(npm dist-tag ls | sed 's/\:.*//')
 
+echo npmtags: $npmtags
+echo branches: $branches
+echo package-name: $package
 
-for tag in $(npm dist-tag ls | sed 's/\:.*//'); do
+for tag in $npmtags; do
   if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
     then
       echo -e "${GREEN}Skipping protected tag, ${BLUE}$tag${GREEN}."
-  elif [[ $(echo "$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')" | grep -e "$tag") ]]
+  elif [[ $(echo $branches | grep -e "$tag") ]]
     then
       echo -e "${GREEN}Keeping tag, ${BLUE}$tag${GREEN}, on NPM because we found a matching branch.${NC}"
   else
