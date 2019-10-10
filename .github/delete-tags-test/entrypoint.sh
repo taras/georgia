c7bce8e6111abd1918484a7fdebd87029bb54067
minkimcello <minkimcello@gmail.com>
2019-10-10 19:03:36 -0400

    test delete

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -9,20 +9,28 @@ BLUE='\033[1;34m'
 NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`"
+
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')"
+branches_masked="$(echo $branches | sed 's/\_/\//g')"
+input_keep_masked="$(echo $INPUT_KEEP | sed 's/\_/\//g')"
 npmtags=$(npm dist-tag ls | sed 's/\:.*//')
 
-for maskedtag in $npmtags; do
-  tag="$(echo $maskedtag | sed 's/\_/\//g')"
-  if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
-    then
-      echo -e "${RED}$tag${GREEN}: Keeping protected tag.${NC}"
-  elif [[ $(echo $branches | grep -e "$tag") ]]
-    then
-      echo -e "${RED}$tag${GREEN}: Keeping tag because we found a matching branch.${NC}"
-  else
-    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-    npm dist-tag rm $package $maskedtag
-    echo -e "${RED}$tag${YELLOW}: Removed tag from NPM because it did not match any existing branches.${NC}"
-  fi
-done
+echo $INPUT_KEEP
+echo $input_keep_masked
+
+# for tag in $npmtags; do
+  
+#   if [[ "$tag" = "latest" ]] || [[ $(echo "$input_keep_masked" | grep -e "$tag") ]]
+#     then
+#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+
+#   elif [[ $(echo $branches_masked | grep -e "$tag") ]]
+#     then
+#       echo -e "${GREEN}: Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
+  
+#   else
+#     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+#     npm dist-tag rm $package $tag
+#     echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
+#   fi
+# done
