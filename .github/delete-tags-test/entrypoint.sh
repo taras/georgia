46b36d16ee20f0feca4e46a03042ebe28236e858
minkimcello <minkimcello@gmail.com>
2019-10-14 18:54:05 -0400

    without quotes

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -10,10 +10,10 @@ NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`";
 input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's:_:__:g;s:\/:_:g')"
-declare -a input_arrayed=("${input_keep_encoded[@]}");
+declare -a input_arrayed=("${$input_keep_encoded[@]}");
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
 branches_encoded="$(echo $branches | sed -E 's:_:__:g;s:\/:_:g')";
-declare -a branches_arrayed=("${"$branches_encoded"[@]}");
+declare -a branches_arrayed=("${$branches_encoded[@]}");
 npmtags=$(npm dist-tag ls | sed 's/:.*//');
 
 for branch in $branches_arrayed
