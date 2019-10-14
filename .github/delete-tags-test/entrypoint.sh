eb02bbc9e75b2d75aa5f675df2ba4fc2bbc85070
minkimcello <minkimcello@gmail.com>
2019-10-14 15:19:32 -0400

    test if array syntax works on ga

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -9,21 +9,33 @@ BLUE='\033[1;34m'
 NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`";
-input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's/\_+/&\_/g;s/\//\_/g')"
+input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's/\_+/&\_/g;s/\//\_/g;s:\s:\n:g')"
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
 branches_encoded="$(echo $branches | sed -E 's/\_+/&\_/g;s/\//\_/g')";
-npmtags=$(npm dist-tag ls | sed 's/\:.*//');
+npmtags=$(npm dist-tag ls | sed 's/:.*//');
 
-for tag in $npmtags; do
-  if [[ "$tag" = "latest" ]] || [[ $(echo "$input_keep_encoded" | grep -e "$tag") ]]
-    then
-      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-  elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
-    then
-      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
-  else
-    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-    npm dist-tag rm $package $tag
-    echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
-  fi
-done
+# for tag in $npmtags; do
+#   if [[ "$tag" = "latest" ]]
+#     then
+#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+#   elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
+#     then
+#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
+#   elif [[ $(echo $(for arg in $input_keep_encoded; do if [[ "$arg" = "$tag" ]]; then echo $tag; else; fi; done;)) ]]
+#     then
+#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+#   else
+#     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+#     npm dist-tag rm $package $tag
+#     echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
+#   fi
+# done
+
+test=("hihi" "hibitch" "yeah")
+for arg in ($test); 
+do 
+  if [[ "$arg" = "hihi" ]]; 
+    then echo "$arg suck"; 
+  else echo "$arg fail"; 
+  fi; 
+done;
