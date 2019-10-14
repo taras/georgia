8d93c468d1765ddba300418ed98a908e0191da9a
minkimcello <minkimcello@gmail.com>
2019-10-14 17:19:58 -0400

    practice run

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -15,28 +15,29 @@ branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
 branches_encoded="$(echo $branches | sed -E 's/\_+/&\_/g;s/\//\_/g')";
 npmtags=$(npm dist-tag ls | sed 's/:.*//');
 
-# for tag in $npmtags; do
-#   if [[ "$tag" = "latest" ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-#   elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
-#   elif [[ $(echo $(for arg in $input_keep_encoded; do if [[ "$arg" = "$tag" ]]; then echo $tag; else; fi; done;)) ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-#   else
-#     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-#     npm dist-tag rm $package $tag
-#     echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
-#   fi
-# done
+for tag in $npmtags; do
+  if [[ "$tag" = "latest" ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+  elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
+  elif [[ $(echo $(for arg in $input_arrayed; do if [[ "$arg" = "$tag" ]]; then echo "$tag"; 
+else; fi; done;)) ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+  else
+    # echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    # npm dist-tag rm $package $tag
+    echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
+  fi
+done
 
-for arg in $input_arrayed; 
-do 
-  if [[ "$arg" = "aewf" ]]; 
-    then echo "$arg yeah"; 
-  else echo "$arg fail"; 
-  fi; 
-done;
+# for arg in $input_arrayed; 
+# do 
+#   if [[ "$arg" = "aewf" ]]; 
+#     then echo "$arg yeah"; 
+#   else echo "$arg fail"; 
+#   fi; 
+# done;
 
