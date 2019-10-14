bfacc3718883e027544a1660aeba18c975e29c1f
minkimcello <minkimcello@gmail.com>
2019-10-14 17:04:40 -0400

    newoaweifja

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -9,7 +9,7 @@ BLUE='\033[1;34m'
 NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`";
-declare -a input_keep_encoded="${(echo $INPUT_KEEP | sed -E 's/\_+/&\_/g;s/\//\_/g;s:\s:\n:g')[@]}"
+declare -a input_keep_encoded=("${"$(echo $INPUT_KEEP | sed -E 's/\_+/&\_/g;s/\//\_/g;s:\s:\n:g')"[@]}")
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
 branches_encoded="$(echo $branches | sed -E 's/\_+/&\_/g;s/\//\_/g')";
 npmtags=$(npm dist-tag ls | sed 's/:.*//');
@@ -31,6 +31,7 @@ npmtags=$(npm dist-tag ls | sed 's/:.*//');
 #   fi
 # done
 
+declare -a test=("${input_keep_encoded[@]}");
 for arg in $input_keep_encoded; 
 do 
   if [[ "$arg" = "aewf" ]]; 
