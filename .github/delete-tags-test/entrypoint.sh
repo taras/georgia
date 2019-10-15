abacaaeb8e1ca6e294beb4b2bbdf3f2fca37e619
minkimcello <minkimcello@gmail.com>
2019-10-15 12:46:29 -0400

    should work

--- .github/delete-tags-test/entrypoint.sh
+++ .github/delete-tags-test/entrypoint.sh
@@ -10,32 +10,37 @@ NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`";
 input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's:_:__:g;s:\/:_:g')"
-# declare -a input_arrayed=("${$input_keep_encoded[@]}");
+declare -a input_arrayed=($(echo $input_keep_encoded | tr " " "\n"))
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
 branches_encoded="$(echo $branches | sed -E 's:_:__:g;s:\/:_:g')";
-declare -a branches_arrayed=("${branches_encoded[@]}");
+declare -a branches_arrayed=($(echo $branches_encoded | tr " " "\n"));
 npmtags=$(npm dist-tag ls | sed 's/:.*//');
 
+
+
+declare -a input_arrayed2=("${$input_keep_encoded[@]}");
+declare -a branches_arrayed2=("${branches_encoded[@]}");
 newarray=($(echo $branches_encoded | tr " " "\n"))
+
 
 # for branch in $branches_arrayed; do if [[ "$branch" = "release-1.0.0" ]]; then echo yeah; fi; done;
 # for branch in $branches_arrayed; do echo "$branch a"; done;
 
 for branch in ${newarray[@]}; do echo "$branch yeah"; done;
 
-# for tag in $npmtags; do
-#   if [[ "$tag" = "latest" ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-#   elif [[ $(echo $(for branch in $branches_arrayed; do if [[ "$branch" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
-#   elif [[ $(echo $(for arg in $input_arrayed; do if [[ "$arg" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
-#     then
-#       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-#   else
-#     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-#     npm dist-tag rm $package $tag
-#     echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
-#   fi
-# done
+for tag in $npmtags; do
+  if [[ "$tag" = "latest" ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+  elif [[ $(echo $(for branch in $branches_arrayed2; do if [[ "$branch" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
+  elif [[ $(echo $(for arg in $input_arrayed; do if [[ "$arg" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+  else
+    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    npm dist-tag rm $package $tag
+    echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
+  fi
+done
