254eff4e006e9ec16dc9169778eb1c6eb5cd6351
minkimcello <minkimcello@gmail.com>
2019-10-16 13:37:58 -0400

    not

--- .github/testargs/entrypoint.sh
+++ .github/testargs/entrypoint.sh
@@ -6,7 +6,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-  then echo default npm publish
+  then npm run yeah ding dong
 else
-  echo $INPUT_NPM_PUBLISH and args
+  npm run $INPUT_NPM_PUBLISH boop
 fi
