1a3f20e89cfeebfd06766c36d6ce9aeab0fb8518
minkimcello <minkimcello@gmail.com>
2019-10-16 13:49:04 -0400

    tester-1

--- .github/testargs/entrypoint.sh
+++ .github/testargs/entrypoint.sh
@@ -8,9 +8,8 @@ NC='\033[0m'
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
 npm config set unsafe-perm true
 npm install
-tag="$(echo $GITHUB_HEAD_REF | sed -E 's/\_+/&\_/g;s/\//\_/g')"
 if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-  then npm publish --access=public --tag $tag
+  then npm publish --access=public --tag "tester-1"
 else
-  npm run $INPUT_NPM_PUBLISH --access=public --tag "tester"
+  npm run $INPUT_NPM_PUBLISH --access=public --tag "tester-2"
 fi
