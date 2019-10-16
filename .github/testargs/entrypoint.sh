5493d3c3538b7d3dc617e67eee8de2362cfb1bc0
minkimcello <minkimcello@gmail.com>
2019-10-16 13:47:44 -0400

    branch publish

--- .github/testargs/entrypoint.sh
+++ .github/testargs/entrypoint.sh
@@ -5,8 +5,12 @@ RED='\033[1;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+npm config set unsafe-perm true
+npm install
+tag="$(echo $GITHUB_HEAD_REF | sed -E 's/\_+/&\_/g;s/\//\_/g')"
 if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-  then npm run yeah ding dong
+  then npm publish --access=public --tag $tag
 else
-  npm run $INPUT_NPM_PUBLISH boop
+  npm run $INPUT_NPM_PUBLISH --access=public --tag "tester"
 fi
