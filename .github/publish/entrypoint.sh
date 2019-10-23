b6a05afa57e09601d0ae63aea20b7e5c0f1f51f1
minkimcello <minkimcello@gmail.com>
2019-10-23 17:50:09 -0400

    yeah

--- .github/publish/entrypoint.sh
+++ .github/publish/entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 
 #npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
-echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
+# echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
 #npm publish
-npm view @minkimcello/georgia@1.10.15-y-7761089
+# npm view @minkimcello/georgia@1.10.15-y-7761089
+
+echo $INPUT_PACKAGES
+
+for animal in $INPUT_PACKAGES; do
+echo "hi $animal"
+done;
