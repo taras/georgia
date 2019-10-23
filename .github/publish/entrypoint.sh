b2b0f94efce95b0c2f36e62573317a25fde364df
minkimcello <minkimcello@gmail.com>
2019-10-23 12:26:45 -0400

    manual tag

--- .github/publish/entrypoint.sh
+++ .github/publish/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > ~/.npmrc
 npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
-tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
-npm publish --access=public --tag $tag
+#tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
+npm publish --access=public --tag "gitreg"
