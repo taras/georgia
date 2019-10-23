e0d31c505eaa4692695606213ca2755099ef1afb
minkimcello <minkimcello@gmail.com>
2019-10-23 13:22:46 -0400

    view

--- .github/publish/entrypoint.sh
+++ .github/publish/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`"
+#npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
 echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
-npm publish
+#npm publish
+npm view @minkimcello/georgia@1.10.15-y-7761089
