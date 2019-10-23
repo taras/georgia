57b9f997eea2d188eb590279035219be66c66150
minkimcello <minkimcello@gmail.com>
2019-10-23 13:07:01 -0400

    echo

--- .github/publish/entrypoint.sh
+++ .github/publish/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 
-# npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
 tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
-echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
-npm publish --tag $tag
+echo tag: $tag
+echo version: "`node -e \"console.log(require('./package.json').version)\"`"
+#echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
+#npm publish --tag $tag
