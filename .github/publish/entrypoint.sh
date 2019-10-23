ad777862988ed9f5d8479f13cac7e2cc9042e824
minkimcello <minkimcello@gmail.com>
2019-10-23 12:24:39 -0400

    publish with tag

---
+++ .github/publish/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > ~/.npmrc
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
+tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
+npm publish --access=public --tag $tag
