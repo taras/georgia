8312701efd0824be73f1cfad03a10d52ec99a1b9
minkimcello <minkimcello@gmail.com>
2019-10-23 13:10:55 -0400

    try without the tag

--- .github/publish/entrypoint.sh
+++ .github/publish/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
-tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`"
 echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
-npm publish --tag "gitreg"
+npm publish
