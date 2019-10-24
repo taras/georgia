c0af84c2d3581f162d43a2da8ef652b2d28eaafc
minkimcello <minkimcello@gmail.com>
2019-10-24 16:29:00 -0400

    trythis

--- .github/commenter/entrypoint.sh
+++ .github/commenter/entrypoint.sh
@@ -28,10 +28,12 @@ function maybe(){
 }
 
 function drop(name, body){
-  return `<details><summary>${name}</summary>${body}</details>`
+  const install_tag = `$ npm install ${name}@${pjson.tag}`
+  const or_line = `or doo doo`
+  return `<details><summary>${name}</summary>Install using the following command:\n\`\`\`bash\n${install_tag}\n\`\`\`\n${or_line}</details>`
 }
 
-const first_line = `The packages of this pull request has been released to Github Package Registry with the tag \`${pjson.tag}\`.`;
+const first_line = `The packages of this pull request has been released to Github Package Registry.`;
 const second_line = `Click on the following packages for instructions on how to install them:`;
 
 markdown(`${first_line}\n${second_line}\n${maybe()}\nbyebye`)
@@ -42,18 +44,10 @@ yarn global add danger --dev
 export PATH="$(yarn global bin):$PATH"
 danger ci
 
-# npm install @minkimcello/georgia@gitreg
-
-# const branch = process.env.GITHUB_HEAD_REF;
-# const masked = branch.replace(/\//g, '_');
-
-# const install_version = `npm install ${pjson.name}@${pjson.version}`;
-
-# const install_tag = `$ npm install ${pjson.name}@${masked}`;
 # const fourth_line = `or by updating your package.json to:`
 # const update_json = `\{\n  \"${pjson.name}\": \"${masked}\"\n\}`
 
-# markdown(`${first_line}\n${second_line}\n\`\`\`bash\n${install_tag}\n\`\`\`\n${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
+# markdown(`${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
 
 
 
